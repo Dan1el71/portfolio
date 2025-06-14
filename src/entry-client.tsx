fb13dea2d8769cde12c0b503088a4ea9350e3b24
Dan1el71 <ddrmhk1@gmail.com>
2025-06-14 11:22:16 -0500

    refactor: remove server-side rendering setup and client entry points

--- src/entry-client.tsx
+++
@@ -1,6 +0,0 @@
-import './styles/index.css'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './i18n/i18n.ts'
-
-ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />)
