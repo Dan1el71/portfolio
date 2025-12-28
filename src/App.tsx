5a7d7859904b84bc6fb4db2bafdb459ad4a28b66
RubenMF99 <rumarfo6@gmail.com>
2025-12-28 18:07:40 -0500

    chore: Initial app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,12 @@
-import StarsBackground from './components/Layout/StarsBackground'
-import Header from './components/Layout/Header'
-import Section from './components/Layout/Section'
-import Hero from './components/Sections/Hero'
-import Projects from './components/Sections/Projects'
-import { useTranslation } from 'react-i18next'
-import AboutMe from './components/Sections/AboutMe'
-import Contact from './components/Sections/Contact'
-import Skills from './components/Sections/Skills'
-import Footer from './components/Layout/Footer'
+import Footer from './layout/Footer'
+import Header from './layout/Header'
+import HomePage from './pages/HomePage'
 
 function App() {
-  const { t } = useTranslation()
-
   return (
     <>
-      <StarsBackground />
       <Header />
-      <main className="px-4">
-        <Section className="py-28 md:py-36">
-          <Hero />
-        </Section>
-        <div className="space-y-24">
-          <Section
-            id="projects"
-            icon="bi bi-briefcase"
-            title={t('header.navItems.projects')}
-          >
-            <Projects />
-          </Section>
-          <Section
-            id="skills"
-            icon="bi bi-tools"
-            title={t('header.navItems.skills')}
-            className="flex flex-col items-center"
-          >
-            <Skills />
-          </Section>
-          <Section
-            id="about-me"
-            icon="bi bi-person"
-            title={t('header.navItems.aboutMe')}
-          >
-            <AboutMe />
-          </Section>
-          <Section id="contact">
-            <Contact />
-          </Section>
-        </div>
-      </main>
+      <HomePage />
       <Footer />
     </>
   )
